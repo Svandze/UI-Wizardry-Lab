9dc58201c9718504e10788cc1b07316fec9afc8b
julianmateocastro <julianmateocastrogaviria@gmail.com>
2024-03-25 11:58:23 -0500

    Comments

--- src/main/java/utils/ElementUtils.java
+++ src/main/java/utils/ElementUtils.java
@@ -17,4 +17,15 @@ public class ElementUtils {
     public void sendKeys(WebElement element, String text) {
         element.sendKeys(text);
     }
+
+    //visible present click
+    //anotacion que busque por cada tipo de locator
+    //enviar lista locators y interactuar con el primero
+    //si es visible o esta presente que escriba en el elemento
+    //un metodo padre que busque si el elemento este presente
+    //verificiar el texto
+    //espera hasta que el elemento sea clickeable
+    // metodo para manejar alertas https://the-internet.herokuapp.com/javascript_alerts ejemplo
+    // extent reporter
+
 }
