f397ab9ce7c7ce9c782a4cad5effa940a29a0a72
Mateo A <mateoarcilatoro@gmail.com>
2024-03-25 14:05:30 -0500

    Setup Junit

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,39 +1,27 @@
 package base;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import core.DriverManager;
 
 
 public class BaseTest {
-    protected static ExtentReports extent;
-    protected ExtentTest test;
     protected WebDriver driver;
 
     @BeforeClass
-    public void setUp() {
-        extent = new ExtentReports();
-        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("target/extent-reports/report.html");
-        extent.attachReporter(htmlReporter);
+    public static void setUp() {
     }
 
     @AfterClass
-    public void tearDown() {
-        extent.flush();
+    public static void tearDown() {
     }
 
     @Before
     public void beforeMethod() {
-        test = extent.createTest(getClass().getSimpleName());
         driver = DriverManager.getDriver();
     }
 
@@ -42,11 +30,5 @@ public class BaseTest {
         if (driver != null) {
             driver.quit();
         }
-    }
-
-    public String takeScreenshot() {
-        TakesScreenshot ts = (TakesScreenshot) driver;
-        String screenshotBase64 = ts.getScreenshotAs(OutputType.BASE64);
-        return "data:image/png;base64," + screenshotBase64;
     }
 }
